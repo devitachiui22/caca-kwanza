7deeaf3c887e76af653a288f8cd5d2176679beb8
devitachiui22 <kuandashopp@gmail.com>
2026-02-16 18:17:03 +0100

    Delete backend directory

--- backend/src/routes/auth.routes.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const authController = require('../controllers/auth.controller');
-
-// Prefix: /api/auth
-router.post('/register', authController.register);
-router.post('/login', authController.login);
-
-module.exports = router;
