02cba4e730201cc93780e1058d716f705361d423
devitachiui22 <kuandashopp@gmail.com>
2026-02-16 21:44:34 +0100

    Delete backend directory

--- backend/src/routes/taxi.routes.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-const taxiController = require('../controllers/taxi.controller');
-const authMiddleware = require('../middlewares/auth.middleware');
-
-router.post('/request', authMiddleware, taxiController.requestRide);
-router.post('/accept', authMiddleware, taxiController.acceptRide);
-router.get('/available', authMiddleware, taxiController.getAvailableRides);
-
-module.exports = router;
--- backend/src/routes/wallet.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const controller = require('../controllers/wallet.controller');
-const auth = require('../middlewares/auth.middleware');
-
-router.get('/balance', auth, controller.getBalance);
-router.get('/transactions', auth, controller.getTransactions);
-
-module.exports = router;
